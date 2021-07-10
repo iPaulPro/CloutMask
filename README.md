816cf8431016bdffb0ef8e24ceb1a861a65fd977
Paul Burke <paul@paulburke.co>
2021-07-10 09:59:54 -0400

    Disables links and buttons that don't work while masked as other users

---
+++ README.md
@@ -0,0 +1,15 @@
+This extension adds a button to the header of bitclout.com profile pages, that when pressed, allows you to browse the website as that account.
+
+![Demo](https://i.imgur.com/bAgJTHE.gif)
+
+### Features
+
+✕ It's not possible to take actions on behalf of a masked user.
+
+✓ Here are the things you're able to access:
+1. View a user's wallet
+2. View a user's notifications    
+3. Browse a user's following feed
+4. Peak at the "Update Profile" screen
+
+Made by [$paulburke](https://bitclout.com/u/paulburke)
--- main.js
+++ main.js
@@ -12,6 +12,22 @@ const getIdentityUsers = () => {
   return JSON.parse(users)
 }
 
+const getLoggedInIdentityUser = () => {
+  let identityUsers = getIdentityUsers()
+  if (identityUsers) return identityUsers[getLoggedInPublicKey()]
+  return undefined
+}
+
+const isMaskedUser = (identityUser) => {
+  return identityUser && identityUser['encryptedSeedHex'] === undefined
+}
+
+const isLoggedInAsMaskedUser = () => {
+  const loggedInIdentityUser = getLoggedInIdentityUser()
+  if (loggedInIdentityUser) return isMaskedUser(loggedInIdentityUser)
+  return undefined
+}
+
 const getPublicKeyFromPage = (page) => {
   const keyElement = page.querySelector('.creator-profile__ellipsis-restriction')
   if (!keyElement) return undefined
@@ -50,6 +66,18 @@ const removePublicKeyFromIdentityUsers = (key) => {
   window.location.reload()
 }
 
+const createCloutMaskIconElement = () => {
+  const environment = chrome || browser
+  if (!environment) return
+  const iconUrl = environment.runtime.getURL('images/icon.svg')
+  const img = document.createElement('img')
+  img.width = 16
+  img.height = 16
+  img.alt = "CloutMask Logo"
+  img.src = iconUrl
+  return img
+}
+
 const addCloutMaskButton = (page) => {
   if (!page || page.querySelector('#clout-mask-button')) return
 
@@ -64,30 +92,109 @@ const addCloutMaskButton = (page) => {
 
   const identityUsers = getIdentityUsers()
   const pageIdentityUser = identityUsers[publicKeyFromPage]
-  const userAddedByCloutMask = pageIdentityUser && !pageIdentityUser['encryptedSeedHex']
+  const userAddedByCloutMask = isMaskedUser(pageIdentityUser)
 
-  const iconUrl = chrome.runtime.getURL('images/icon.svg')
+  const icon = createCloutMaskIconElement().outerHTML
   const cloutAsButton = document.createElement('button')
   cloutAsButton.id = 'clout-mask-button'
   cloutAsButton.className = 'btn btn-dark btn-sm text-muted fs-14px rounded-pill'
 
   if (userAddedByCloutMask) {
-    cloutAsButton.innerHTML = `<img src="${iconUrl}" width="16" height="16" alt="CloutMask Logo"> Remove from accounts`
+    cloutAsButton.innerHTML = `${icon} Remove account`
     cloutAsButton.onclick = () => removePublicKeyFromIdentityUsers(publicKeyFromPage)
     topBar.appendChild(cloutAsButton)
   } else if (!pageIdentityUser) {
     cloutAsButton.setAttribute('bs-toggle', 'tooltip')
-    cloutAsButton.innerHTML = `<img src="${iconUrl}" width="16" height="16" alt="CloutMask Logo">`
+    cloutAsButton.innerHTML = icon
     cloutAsButton.title = "Add account with CloutMask"
     cloutAsButton.onclick = () => addPublicKeyToIdentityUsers(publicKeyFromPage)
     topBar.appendChild(cloutAsButton)
   }
 }
 
-const appRootObserverCallback = () => {
+const disableFeedPostButtons = () => {
+  const iconRows = document.querySelectorAll('.js-feed-post-icon-row__container')
+  iconRows.forEach((row) => {
+    const postButtons = Array.from(row.children)
+    postButtons.splice(postButtons.length - 1, 1)
+    postButtons.forEach((button) => {
+      button.style.opacity = '0.5'
+      button.style.pointerEvents = 'none'
+    })
+  })
+}
+
+const disableElement = (element) => {
+  if (!element || !element.style) return
+  element.style.opacity = '0.5'
+  element.style.pointerEvents = 'none'
+  element.disabled = true
+};
+
+const disableAll = (elements) => {
+  disableElement(elements)
+}
+
+const disableKnownLinks = () => {
+  const anchors = document.querySelectorAll(
+    "a[href*='/buy'], a[href*='/sell'], a[href*='/transfer'], a[href*='/select-creator-coin'], a[href*='/send-bitclout'], a[href*='/inbox'], a[href*='/settings'], a[href*='/buy-bitclout'], a[href*='/admin']"
+  )
+  anchors.forEach(disableAll)
+}
+
+const disableClasses = () => {
+  const elements = document.querySelectorAll('.feed-create-post__textarea, .update-profile__image-delete, feed-post-dropdown, app-update-profile-page .btn-primary')
+  elements.forEach(disableAll)
+}
+
+const disablePostButtons = () => {
+  const createPostElement = document.querySelector('feed-create-post')
+  if (createPostElement) {
+    const postButton = createPostElement.querySelector('.btn-primary')
+    disableElement(postButton)
+  }
+}
+
+const disableFollowButtons = (mutationsList) => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      const node = mutation.target
+      if ((node.innerText === 'Unfollow' || node.innerText === 'Follow')) {
+        disableElement(node)
+      }
+    }
+  }
+}
+
+const addMaskToAccountSelector = () => {
+  const accountSelectorMaskIconId = '__clout-mask-account-selector-id'
+  if (document.getElementById(accountSelectorMaskIconId)) return
+  const accountName = document.querySelector('.change-account-selector__ellipsis-restriction')
+  const text = accountName.innerText
+  const icon = createCloutMaskIconElement()
+  icon.id = accountSelectorMaskIconId
+
+  accountName.innerHTML = `${icon.outerHTML} ${text}`
+}
+
+const appRootObserverCallback = (mutationsList) => {
+  if (isLoggedInAsMaskedUser()) {
+    disableFollowButtons(mutationsList)
+    disableFeedPostButtons()
+    disableKnownLinks()
+    disableClasses()
+    disablePostButtons()
+  }
+
   const profilePage = document.querySelector('app-creator-profile-page')
   if (profilePage) {
     addCloutMaskButton(profilePage)
+  }
+}
+
+const bodyObserverCallback = () => {
+  if (isLoggedInAsMaskedUser()) {
+    addMaskToAccountSelector()
   }
 }
 
@@ -97,6 +204,13 @@ const init = () => {
     const appRootObserverConfig = {childList: true, subtree: true}
     const appRootObserver = new MutationObserver(appRootObserverCallback)
     appRootObserver.observe(appRoot, appRootObserverConfig)
+  }
+
+  const body = document.querySelector('body')
+  if (body) {
+    const bodyObserverConfig = { childList: true, subtree: false }
+    const bodyObserver = new MutationObserver(bodyObserverCallback)
+    bodyObserver.observe(body, bodyObserverConfig)
   }
 }
 
